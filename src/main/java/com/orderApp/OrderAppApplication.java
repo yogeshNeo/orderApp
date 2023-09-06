c59e3b368459fbc994d26caf62c2b7caad7d5b0b
Yogesh Choudhary <yogesh.choudhary@neosoftmail.com>
2023-09-06 19:31:59 +0530

    add api gateway

--- src/main/java/com/orderApp/OrderAppApplication.java
+++ src/main/java/com/orderApp/OrderAppApplication.java
@@ -3,8 +3,10 @@ package com.orderApp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @Slf4j
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderAppApplication {
 
